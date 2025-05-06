c25d93526c3869cae3e8ff7f33cd253e0919e597
Frank Purcell <fxpurcell@gmail.com>
2025-05-06 15:54:03 -0700

    feat: new load script

--- scripts/load.sh
+++ scripts/load.sh
@@ -14,33 +14,40 @@ chk=${GTFS_DIR}/${required_feed}.gtfs.zip
 if [ -f $chk ]; then
   echo "INFO: starting the load as file $chk *does* exist."
 
-  # remove old .sql files from gtfs dir
+  echo "step 0: remove old .sql files from gtfs dir"
   rm -f ${GTFS_DIR}/*.sql ${GTFS_DIR}/*schema ${GTFS_DIR}/*views
 
-  # create "current" schema (in addition to gtfs agency schemas)
+  echo "step 1: patch CTRAN_FLEX, etc..."
+  $LDDIR/patch_gtfs.sh
+
+  echo "step 2: create 'current' schema (in addition to gtfs agency schemas)"
   make_schema "current"
   load_schemas
 
-  # grab and load the shape .sql files
+  echo "step 3: grab and load the shape (.sql) files, ala the trimet rail dashed-lines"
   get_shps
   load_shps
 
-  # load gtfs feeds into gtfsdb
+  echo "step 4: load gtfs feeds into gtfsdb"
   for f in ${GTFS_DIR}/*gtfs.zip
   do
     name=$(feed_name_from_zip $f)
 
     cmd="poetry run gtfsdb-load -c -ct -g -d $ott_url -s ${name} ${f}"
-    echo $cmd
+    echo "  $cmd"
     eval $cmd
     sleep 1
   done
+  echo; echo;
 
+  echo "step 5: run the shared stops population"
   cmd="poetry run update-shared-stops -s ${required_feed} -d $ott_url ott/gtfsdb/ext/shared_stops/data/shared_stops.csv"
   echo $cmd
   eval $cmd
   echo; echo;
 
+  echo "step 6: create the 'current' views, etc..."
+  echo "  NOTE: we need views to run after shared-stops, as we then exclude shared stops"
   make_views
   load_views  
   echo;  echo;
