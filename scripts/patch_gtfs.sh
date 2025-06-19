c7862b2237a51362fc2f5ec843a78b68c673b0d9
Frank Purcell <fxpurcell@gmail.com>
2025-06-18 18:33:51 -0700

    feat(patch): test for empty file ... add optional backup patch file

--- scripts/patch_gtfs.sh
+++ scripts/patch_gtfs.sh
@@ -4,19 +4,36 @@
 # November 2024
 #
 function patch() {
-  zip=$1
-  file=$2
-  url=$3
+  local zip=$1
+  local file=$2
+  local url=$3
+  local bkup=$4
+  local exp_size=${5:-100000}
 
   cd ~/gtfs/
   echo $PWD
   curl $url > $file
-  if [ -f "$zip" ]; then
+  local size=`ls -ltr $file | awk -F" " '{ print $5 }'`
+  if [ $size -lt $exp_size ] && [ -f "$bkup" ]; then
+    echo
+    echo "WARN: $url download of $file is only $size bytes in size ... patching $zip with $bkup"
+    echo
+    cp $bkup $file
+  else
+    echo
+    echo "ERROR: $url download of $file is only $size bytes in size ... not patching $zip"
+    echo
+    rm $file
+  fi
+
+  if [ -f "$file" ] && [ -f "$zip" ]; then
     zip $zip $file
   else
-    echo "$zip does not exist"
+    echo
+    echo "$file and/or $zip does not exist .. not patching the .zip"
+    echo
   fi
   cd -
 }
 
-patch CTRAN_FLEX.gtfs.zip locations.geojson "https://services5.arcgis.com/T6Y1WlK8fKvVXSi3/ArcGIS/rest/services/The_Current_Service_Areas/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=pgeojson"
+patch CTRAN_FLEX.gtfs.zip locations.geojson "https://services5.arcgis.com/T6Y1WlK8fKvVXSi3/ArcGIS/rest/services/The_Current_Service_Areas/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=pgeojson" ~/OLD/ctran-flex-june-2025.geojson
