d067782e2d3d4b3f531cc64f0dc74b035f337ced
Frank Purcell <fxpurcell@gmail.com>
2025-11-11 12:26:04 -0800

    fix(pelias): fix up the stops export script

--- scripts/export/pelias.sh
+++ scripts/export/pelias.sh
@@ -1,15 +1,18 @@
 SHPDIR=`dirname $0`
 . $SHPDIR/../base.sh
 
-PELIAS=${1:-"pelias@rj-st-mapgeo01"}
+PELIAS=${1:-"pelias@rj-dv-mapgeo01"}
+DIR=${2:-"~/gtfs"}
 
 # export pelias .csv file for each feed
 for z in ~/gtfs/*zip
 do 
   f=${z##*/}
   f=${f%.gtfs.zip}
-  poetry run pelias-stops -s ${f} "${f}_stops.csv"
+  #poetry run pelias-stops -s ${f} "${f}_stops.csv"
 done
 
 # move to pelias etl server
-scp *_stops.csv "${PELIAS}:~/gtfs/"
+ssh $PELIAS "mkdir -p $DIR"
+scp *_stops.csv "${PELIAS}:$DIR"
+mv *_stops.csv ~/gtfs/
