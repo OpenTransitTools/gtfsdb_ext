8732d9c789af594d5ef705fb7df5ed0ababd36e4
Frank Purcell <fxpurcell@gmail.com>
2025-12-17 15:14:33 -0800

    fix(pelias): move pelias export output to ~/gtfs/

--- scripts/export/pelias.sh
+++ scripts/export/pelias.sh
@@ -5,14 +5,15 @@ PELIAS=${1:-"pelias@rj-dv-mapgeo01"}
 DIR=${2:-"~/gtfs"}
 
 # export pelias .csv file for each feed
-for z in ~/gtfs/*zip
+for z in ${DIR}/*zip
 do 
   f=${z##*/}
   f=${f%.gtfs.zip}
-  poetry run pelias-stops -s ${f} "${f}_stops.csv"
+  poetry run pelias-stops -s ${f} "${DIR}/${f}_stops.csv"
 done
 
-# move to pelias etl server
-ssh $PELIAS "mkdir -p $DIR"
-scp *_stops.csv "${PELIAS}:$DIR"
-mv *_stops.csv ~/gtfs/
+# move to pelias etl server(s)
+for p in $PELIAS
+  ssh $p "mkdir -p $DIR"
+  scp ${DIR}/*_stops.csv "${p}:$DIR/"
+done
