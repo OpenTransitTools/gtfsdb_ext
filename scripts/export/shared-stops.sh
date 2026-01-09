fb43bce7ed5cdcfa18a180f2d863525a57f5b8c2
Frank Purcell <fxpurcell@gmail.com>
2026-01-08 20:37:44 -0800

    fix(export): improve the share stop .sh script

--- scripts/export/shared-stops.sh
+++ scripts/export/shared-stops.sh
@@ -1,14 +1,24 @@
 SDIR=`dirname $0`
 . $SDIR/../base.sh
 
-CSV=ss.csv
-rm -f $CSV
-curl "https://developer.trimet.org/ws/v3/sharedStops?csv=true&appid=8CBD14D520C6026CC7EEE56A9" > $CSV
+ext_data_dir=data/shared_stops
+mkdir -p ${ext_data_dir}
+echo $ott_url
 
-# test data (don't rm -f $CSV above):
-#CSV=${1:-"$SDIR/../../ott/gtfsdb/ext/shared_stops/data/shared_stops.csv"}
+echo " step a: curl the shared stops"
+SSCSV="${ext_data_dir}/shared_stops.csv"
+rm -f $SSCSV
+curl "https://developer.trimet.org/ws/v3/sharedStops?csv=true&appid=8CBD14D520C6026CC7EEE56A9" > $SSCSV
+echo
 
-cmd="poetry run echo-shared-stops -d $ott_url $CSV"
+echo " step b: shared stops report"
+cmd="poetry run shared-stops-report -d $ott_url ${SSCSV} > ${ext_data_dir}/shared_stops.html 2> /dev/null"
 echo $cmd
-sleep 2
 eval $cmd
+echo
+
+echo " step c: more the shared stops"
+cmd="poetry run echo-shared-stops -d $ott_url ${SSCSV} > ${ext_data_dir}/shared_stops.txt 2> /dev/null"
+echo $cmd
+eval $cmd
+more "${ext_data_dir}/shared_stops.txt"
