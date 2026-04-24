4117fa5e51d7f7f90c9bcffa028570ba1cb575d8
Frank Purcell <fxpurcell@gmail.com>
2026-04-24 15:39:55 -0700

    fix(format): clean up

--- ott/gtfsdb/ext/query.py
+++ ott/gtfsdb/ext/query.py
@@ -34,13 +34,12 @@ def to_stop_dict(rec, i=0, feed_id=None, def_agency=None, table="CURRENT_STOPS",
 
 
 def set_shared_stop(db, shared_stops_val, feed_id, stop_id, table="STOPS"):
-    sql = "UPDATE {}.{} SET shared_stops = '{}' WHERE stop_id = '{}'".format(feed_id, table, shared_stops_val, stop_id)
+    sql = f"UPDATE {feed_id}.{table} SET shared_stops = '{shared_stops_val}' WHERE stop_id = '{stop_id}'"
     return do_sql(db, sql)
 
 
 def find_stop(db, feed_id, stop_id, table="STOPS"):
-    #import pdb; pdb.set_trace()
-    sql = "select * from {}.{} where stop_id = '{}'".format(feed_id, table, stop_id)
+    sql = f"select * from {feed_id}.{table} where stop_id = '{stop_id}'"
     return do_sql(db, sql)
 
 
@@ -50,22 +49,23 @@ def stop_distance(db, feed_id_a, stop_id_a, feed_id_b, stop_id_b):
     :return: distance in meters
     :note: https://postgis.net/docs/ST_DistanceSphere.html
     """
-    sql = "select ST_DistanceSphere(a.geom, b.geom) from {}.stops a, {}.stops b where a.stop_id = '{}' and b.stop_id = '{}'".format(feed_id_a, feed_id_b, stop_id_a, stop_id_b)
+    sql = f"select ST_DistanceSphere(a.geom, b.geom) from {feed_id_a}.stops a, {feed_id_b}.stops b where a.stop_id = '{stop_id_a}' and b.stop_id = '{stop_id_b}'"
     return do_sql(db, sql)
 
 
 def nearest_stops(db, feed_id, stop_id, src_feed_id, table="STOPS", limit=10):
-  sql = "select ST_DistanceSphere(a.geom, b.geom) as meters_apart, a.* from {}.{} a, {}.stops b where b.stop_id = '{}' order by 1 limit {}".format(feed_id, table, src_feed_id, stop_id, limit)
+  sql = f"select ST_DistanceSphere(a.geom, b.geom) as meters_apart, a.* from {feed_id}.{table} a, {src_feed_id}.stops b where b.stop_id = '{stop_id}' order by 1 limit {limit}"
   return do_sql(db, sql)
 
 
 def nearest(db, feed_id, stop_id, dist, src_feed_id, table="STOPS"):
-    sql = "select * from {}.{} stop where ST_DWithin(stop.geom, (select t.geom from {}.stops t where stop_id = '{}'), {})".format(feed_id, table, src_feed_id, stop_id, dist)
+    #import pdb; pdb.set_trace()
+    sql = f"select * from {feed_id}.{table} where ST_DWithin(stop.geom, (select t.geom from {src_feed_id}.stops t where stop_id = '{stop_id}'), {dist})"
     return do_sql(db, sql)
 
 
 def agencies(db, feed_id, limit=50, table="AGENCY"):
-    sql = "select * from {}.{} limit {}".format(feed_id, table, limit)
+    sql = f"select * from {feed_id}.{table} limit {limit}"
     return do_sql(db, sql)
 
 
